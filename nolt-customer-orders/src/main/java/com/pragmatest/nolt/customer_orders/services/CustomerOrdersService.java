a570f27665dd65613a5c9bf15eeb7964553dc6d6
clydevassallo <cooler364@gmail.com>
2022-05-26 23:55:36 +0200

    feat: implement get in service layer

--- nolt-customer-orders/src/main/java/com/pragmatest/nolt/customer_orders/services/CustomerOrdersService.java
+++ nolt-customer-orders/src/main/java/com/pragmatest/nolt/customer_orders/services/CustomerOrdersService.java
@@ -1,8 +1,10 @@
 package com.pragmatest.nolt.customer_orders.services;
 
 import com.pragmatest.nolt.customer_orders.services.models.Order;
+import com.pragmatest.nolt.customer_orders.services.models.OrderItem;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -13,5 +15,19 @@ public class CustomerOrdersService {
         return order.getId();
     }
 
-    // TODO - 1. Implement getOrder(id) method that returns a hardcoded com.pragmatest.nolt.customer_orders.services.models.Order.
+    public Order getOrder(String orderId, String customerId) {
+        Order order = new Order();
+        order.setCustomerId(customerId);
+
+        OrderItem orderItem = new OrderItem();
+        orderItem.setQuantity(1);
+        orderItem.setMenuItemId("burger");
+        orderItem.setNotes("extra lettuce");
+
+        List<OrderItem> orderItems = List.of(orderItem);
+
+        order.setOrderItems(orderItems);
+        order.setId(orderId);
+        return order;
+    }
 }
